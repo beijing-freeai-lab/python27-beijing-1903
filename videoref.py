629159f7044ae307bfc40b517f0b207bc53c4267
40660367 <30319701+40660367@users.noreply.github.com>
2019-02-26 22:14:07 +0800

    Create videoref.py

---
+++ videoref.py
@@ -0,0 +1,11 @@
+from IPython.display import HTML,display
+from IPython.display import IFrame
+a1=HTML('<iframe src="//player.bilibili.com/player.html?aid=14184325&cid=23153678&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="100%" height=480> </iframe>')
+a2=IFrame('//player.bilibili.com/player.html?aid=44134883&cid=77304853&page=1', width='100%', height=680)
+
+html1='''
+%%html
+<h1>数据结构入门</h1>
+<hr>
+<iframe src="//player.bilibili.com/player.html?aid=14184325&cid=23153678&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="100%" height=480> </iframe>
+'''
